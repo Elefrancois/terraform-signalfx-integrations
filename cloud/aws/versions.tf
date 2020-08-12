71c016afc8d175b06ed259af50ed02c31a188fd6
Emilien Lefrancois <emilien.lefrancois@fr.clara.net>
2020-08-12 15:28:22 +0200

    fix

---
+++ cloud/aws/versions.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    signalfx = {
+      source = "terraform-providers/signalfx"
+    }
+  }
+  required_version = ">= 0.13"
+}
